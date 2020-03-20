d7bf6920ded28be80e646d506f52e69acd2a8e2b
platuro <30547642+palturo@users.noreply.github.com>
2020-03-20 15:40:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,21 @@
 const BalanceBoard = require("wii-balance-board-pi");
-/*const raspi = require('raspi');
-const Serial = require('raspi-serial').Serial;
-
-raspi.init(() => {
-    console.log("Init Pi");
-    var serial = new Serial({baudRate: 9600});
-    serial.open(() => {
-        console.log("Serial Open");
-      serial.on('data', (data) => {
-        process.stdout.write(data);
-      });
-      serial.write('Hello from raspi-serial');
-      function SendTest(){
-        serial.write('Hello from raspi-serial');
-        console.log("Sende Nachricht");
-      }
-      setInterval(SendTest, 1000);
-    });
-  });*/
+var express = require("express");
+var app = express();
 
 var balanceBoard = new BalanceBoard();
+
+var BalanceData = null;
  
 balanceBoard.connect();
  
 balanceBoard.on("data", data => {
-  console.log(data);
+  BalanceData = data;
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+app.get("/weight", (req, res, next) => {
+  res.json(BalanceData);
 });
